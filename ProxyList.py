5bf6552d20825124e524ee647e3eb13689478428
LiDong-96 <57519547+LiDong-96@users.noreply.github.com>
2020-03-22 10:05:55 +0100

    Delete ProxyList.py

--- ProxyList.py
+++
@@ -1,8 +0,0 @@
-#-*-coding:utf-8-*-
-
-# List of Socks4/5 proxies, with (IP address: port) packed in tuples.
-SocksProxyList = [("xxx.xx.xx.xxx", "xxxx")]
-
-# These are nice proxy-provided websites:
-# https://sockslist.net/list/proxy-socks-5-list/
-# https://www.proxynova.com/proxy-server-list/
